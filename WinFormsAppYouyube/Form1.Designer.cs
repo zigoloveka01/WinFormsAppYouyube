3058d1298618e9c48a466e55475ecda134b66c7b
zigoloveka01 <zigoloveka1@gmail.com>
2025-01-06 09:04:05 +0700

    Add project files.

---
+++ WinFormsAppYouyube/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WinFormsAppYouyube
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            checkedListBox1 = new CheckedListBox();
+            button1 = new Button();
+            button2 = new Button();
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.CheckOnClick = true;
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(28, 27);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(193, 268);
+            checkedListBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(250, 74);
+            button1.Name = "button1";
+            button1.Size = new Size(109, 49);
+            button1.TabIndex = 1;
+            button1.Text = ">>>";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(250, 159);
+            button2.Name = "button2";
+            button2.Size = new Size(109, 47);
+            button2.TabIndex = 2;
+            button2.Text = "<<<";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(395, 27);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(167, 264);
+            listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(checkedListBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private CheckedListBox checkedListBox1;
+        private Button button1;
+        private Button button2;
+        private ListBox listBox1;
+    }
+}
---
+++ WinFormsAppYouyube/Form1.cs
@@ -0,0 +1,48 @@
+﻿namespace WinFormsAppYouyube
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] products = { "เหล้า", "เบียร์", "โซดา", "น้ำอัดลม", "กาแฟ", "น้ำเปล่า" };
+            checkedListBox1.Items.AddRange(products);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {//ปุ่ม >>>
+            if (checkedListBox1.CheckedItems.Count == 0)
+            {
+                return;
+            }
+            // อ่านรายการข้อมูล
+            foreach (string iten in checkedListBox1.CheckedItems)
+            {
+                if (listBox1.FindStringExact(iten) == -1)
+                {
+                    if (listBox1.FindStringExact(iten) == -1)
+                    {
+                        listBox1.Items.Add(iten);
+                    }
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItems.Count == 0)
+            {
+                return;
+            }
+            while(listBox1.SelectedIndex!= -1)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+        }
+    }
+}
